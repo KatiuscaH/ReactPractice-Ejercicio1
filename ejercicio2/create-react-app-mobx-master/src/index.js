d8d5471d158b64c7e402c4914b627a1b6ba7ed32
Katiusca Herrera <katiusca.herrera@gmail.com>
2018-05-09 17:25:43 -0500

    Ejercicio 2 usando Mobx Creando un contador

--- ejercicio2/create-react-app-mobx-master/src/index.js
+++ ejercicio2/create-react-app-mobx-master/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import Counter from './Counter';
 //import App from './App';
-import './index.css';
+//import './index.css';
 import Principal from './Principal';
 
 ReactDOM.render(
