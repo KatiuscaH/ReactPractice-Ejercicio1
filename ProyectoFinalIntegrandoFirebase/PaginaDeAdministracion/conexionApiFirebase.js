058526e6514f2d1171e6721d0faeb9fa7002e65d
Katiusca Herrera <katiusca.herrera@gmail.com>
2018-05-19 18:00:26 -0500

    Deploy final Firebase

---
+++ ProyectoFinalIntegrandoFirebase/PaginaDeAdministracion/conexionApiFirebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDvjMMCfTa76r0hl0D12y5S3YFwenKVReE",
+    authDomain: "administracion-7113b.firebaseapp.com",
+    databaseURL: "https://administracion-7113b.firebaseio.com",
+    projectId: "administracion-7113b",
+    storageBucket: "administracion-7113b.appspot.com",
+    messagingSenderId: "730981297008"
+};
+firebase.initializeApp(config);
