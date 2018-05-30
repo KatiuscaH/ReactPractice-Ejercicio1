939c87041a1f247b41d0683c178aaf8720b6462c
KatiuscaH <katiusca.herrera@gmail.com>
2018-05-30 12:01:34 -0500

    update firebase controller

--- ProyectoFinalIntegrandoFirebase/ProyectoFinal-ReactRouter-v4-Firebase/src/firebaseController - copia.js
+++ ProyectoFinalIntegrandoFirebase/ProyectoFinal-ReactRouter-v4-Firebase/src/firebaseController - copia.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyDvjMMCfTa76r0hl0D12y5S3YFwenKVReE",
-    authDomain: "administracion-7113b.firebaseapp.com",
-    databaseURL: "https://administracion-7113b.firebaseio.com",
-    projectId: "administracion-7113b",
-    storageBucket: "administracion-7113b.appspot.com",
-    messagingSenderId: "730981297008"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 
 firebase.initializeApp(config);
